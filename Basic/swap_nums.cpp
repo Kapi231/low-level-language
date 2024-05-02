1b6d9f64bd7e7e13989dd6236ca618b53e72636f
Kapi231 <89849620+Kapi231@users.noreply.github.com>
2024-05-02 13:50:16 +0200

    removing unnessecery lines

--- Basic/swap_nums.cpp
+++ Basic/swap_nums.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 
 void swap_nums(int &x, int &y) {
-	
 	int z = x;
 	x = y;
 	y = z;
 }
 
 int main() {
-	
 	int first_num = 10;
 	int second_num = 30;
 	std::cout << "before change:\n" << first_num << "\n" << second_num;
-
 	swap_nums(first_num, second_num);
-
 	std::cout << "\n\nafter change:\n" << first_num << "\n" << second_num;
-
-
 	return 0;
 }
