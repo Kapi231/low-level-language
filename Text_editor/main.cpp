270da420c579459161f21cb128b56488b75436af
Kapi231 <89849620+Kapi231@users.noreply.github.com>
2024-08-24 21:59:25 +0200

    Cursor
    moving cursor

--- Text_editor/main.cpp
+++ Text_editor/main.cpp
@@ -7,18 +7,43 @@ int main()
   std::vector<char> buffer{};
   
   int cur_x, cur_y;
+  int pos_x, pos_y;
 
   initscr();
   keypad(stdscr, 1);
   noecho();
-
+  getyx(stdscr, pos_x, pos_y);
 
   while (1) 
   {
     getyx(stdscr, cur_y, cur_x);
-    char x = getch();
+    int x = getch();
 
-    buffer.insert(buffer.end(), x);
+    switch (x) {
+      case KEY_LEFT:
+        pos_x--;
+        x = 0;
+        break;
+      case KEY_RIGHT:
+        pos_x++;
+        x = 0;
+        break;
+      case KEY_UP:
+        pos_y++;
+        x = 0;
+        break;
+      case KEY_DOWN:
+        pos_y--;
+        x = 0;
+        break;
+      default:
+        pos_x++;
+        break;
+    }
+    move(pos_x, pos_y);
+
+    refresh();   
+    buffer.insert(buffer.begin() + pos_x - 1, x);
     
     //Clears screen after every display
     clear();
