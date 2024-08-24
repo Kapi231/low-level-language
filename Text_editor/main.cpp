509e303abf6068b5e8849b267a390665971ea40d
Kapi231 <89849620+Kapi231@users.noreply.github.com>
2024-08-24 20:48:55 +0200

    buffer and display
    Loading characters on to buffer and to display

--- Text_editor/main.cpp
+++ Text_editor/main.cpp
@@ -1,8 +1,11 @@
 #include <cstdlib>
 #include <curses.h>
+#include <vector>
 
 int main() 
 {
+  std::vector<char> buffer{};
+
   initscr();
   keypad(stdscr, 1);
   noecho();
@@ -10,9 +13,20 @@ int main()
   while (1) 
   {
     char x = getch();
+
+    buffer.insert(buffer.end(), x);
     
-    printw("%c", x);
+    //Clears screen after every display
+    clear();
+    refresh();
+
+    //Display what is written on the buffer
+    for (int i = 0; i < buffer.size(); i++)
+    {
+      printw("%c", buffer[i]);
+    }
   }
-    endwin();
+  endwin();
+
   return 0;
 }
